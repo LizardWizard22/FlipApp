fb2e42a6ef297b4ef0e6eae93baf4c38b065f675
Kevin <4514745@RTS-365.dk>
2018-04-24 12:39:51 +0200

    Flip Back
    ----[Changes]----
    +if you fail the card wil automatically flip back

--- java.js
+++ java.js
@@ -2,12 +2,32 @@ var board = document.getElementById('board');
 var tilesflipped = [];
 var tilesMatch = [];
 
+/*function EndOfGame(){
+    if(board.querySelectorAll('section').length === board.querySelectorAll('reward').length)
+    document.getElementById('message').classList.add('show')
+}*/
+
+function flipBack() {
+     var tiles = board.querySelectorAll('section');
+     tiles[tilesflipped[0]].classList.remove('flipped')
+     tiles[tilesflipped[1]].classList.remove('flipped')
+     tilesflipped =  [];
+     tilesMatch = [];
+     setTimeout(EndOfGame, 500)
+     board.style.pointerEvents = 'auto';
+}
+
 function twoTiles(tiles){
   if(tilesflipped.length >= 2 ){
       board.style.pointerEvents = 'none';
       if (tilesMatch[0] === tilesMatch[1]){
       tiles[tilesflipped[0]].classList.add('reward')
       tiles[tilesflipped[1]].classList.add('reward')
+      tilesflipped = [];
+      tilesMatch = [];
+      board.style.pointerEvents = 'auto';
+    } else {
+        setTimeout(flipBack, 1500)
     }
   }
 }
