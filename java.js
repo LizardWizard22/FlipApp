31403f3089fe4e7cea1a68611e9e2808ce2fbe72
Unknown <4514745@RTS-365.dk>
2018-04-24 09:57:02 +0200

    Gource

--- java.js
+++ java.js
@@ -1,13 +1,18 @@
 var board = document.getElementById('board');
 var tilesflipped = [];
+var tilesMatch = [];
 
 function fliptile(event){
     'use strict';
     var tiles = Array.from(board.querySelectorAll('section'));
   if(event.target !== event.currentTarget) {
+      if(!event.target.parentNode.classList.contains('flipped')){
      event.target.parentNode.classList.add('flipped');
      tilesflipped.push(tiles.indexOf(event.target.parentNode));
      console.log(tilesflipped);
+     tilesMatch.push(event.target.nextElementSibling.innerHTML)
+     console.log(tilesMatch)
+      }
    }
 }
 
