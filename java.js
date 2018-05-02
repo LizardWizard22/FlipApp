9aa0cba787f0bc688a3e2572fdd722763ec330ca
Kevin <4514745@RTS-365.dk>
2018-05-02 13:51:50 +0200

    Major Bug Fixing

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-var tileImgs = ['A','A','B', 'B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L'];
+var tileImgs = ['A','B','C','D','E','F','G','H','I','J','K','L'];
 var board = document.getElementById('board');
 var tilesflipped = [];
 var tilesMatch = [];
@@ -6,43 +6,46 @@ var i;
 
 function drawBoard(event){
     event.preventDefault();
-    document.getElementById('welcome').style.display = 'none';
+    document.getElementById('welcome').style.display = 'none'
     board.style.display = 'flex';
-
     var gameTiles = document.getElementById('playgame').level.value;
     console.log(gameTiles);
     var gameTileImgs = tileImgs.slice(0,gameTiles/2); 
     gameTileImgs = gameTileImgs.doubleShuffle();
+    
 
-    for(i = 0; 1 < gameTileImgs.length; i += 1){
+    for(i = 0; i < gameTileImgs.length; i += 1){
+        console.log(i);
         var content = '';
         content += '<section>'
         content += '<div class="front"></div>'
-        content += '<div class="back">' + gameTilesImg[i] + '</div>'
+        content += '<div class="back">' + gameTileImgs[i] + '</div>'
         content += '</section>';
 
         board.insertAdjacentHTML('beforeend', content);
     }
+    alert('er du her?')
 }
 
 Array.prototype.doubleShuffle = function (){
     var d;
     for(d=0; d<this.length; d = d+2) {
-     this.splice(D+1, 0, this[d])
+     this.splice(d+1, 0, this[d])
     }
     console.log(this);
     var i = this.length;
     var j;
     var temp;
     while(--i > 0 ){
-        j = Math.floor(Math.random() * (1+i));
-        console.log(j)
+        
+        j = Math.floor(Math.random() * i + 1);
+        console.log(j);
         temp = this[j];
-        this [j] = this[i]
-        thi[i] = temp;
-
+        console.log(temp);
+        this[j] = this[i]
+        this[i] = temp;
     }
-    console.log[this];
+
     return this;
 }
 
@@ -55,16 +58,15 @@ document.getElementById('message').classList.remove('show');
 
 function EndOfGame(){
     if(board.getElementsByTagName('section').length === board.getElementsByClassName('reward').length){
-    document.getElementById('message').classList.add('show');
+        document.getElementById('message').classList.add('show');
+    }
 }
-
 function flipBack() {
      var tiles = board.querySelectorAll('section');
      tiles[tilesflipped[0]].classList.remove('flipped');
      tiles[tilesflipped[1]].classList.remove('flipped');
      tilesflipped =  [];
      tilesMatch = [];
-     setTimeout(EndOfGame, 500);
      board.style.pointerEvents = 'auto';
 }
 
@@ -76,6 +78,7 @@ function twoTiles(tiles){
       tiles[tilesflipped[1]].classList.add('reward');
       tilesflipped = [];
       tilesMatch = [];
+      setTimeout(EndOfGame, 500);
       board.style.pointerEvents = 'auto';
     } else {
         setTimeout(flipBack, 1500)
@@ -86,7 +89,7 @@ function twoTiles(tiles){
 function fliptile(event){
     'use strict';
     var tiles = Array.from(board.querySelectorAll('section'));
-  if(event.target !== event.currentTarget) {
+  if(event.target !== event.currentTarget && event.touches.length === 1) {
       if(!event.target.parentNode.classList.contains('flipped')){
      event.target.parentNode.classList.add('flipped');
      tilesflipped.push(tiles.indexOf(event.target.parentNode));
@@ -98,8 +101,7 @@ function fliptile(event){
    }
 }
 
-document.addEventListener('DOMContentLoaded',function () {
-    'use strict';
-board.addEventListener('touchend', fliptile);
-document.getElementById('message').getElementsByTagName('button')[0].addEventListener('click, newGame');
-})}
+board.addEventListener('touchstart', fliptile);
+document.getElementById('message').getElementsByTagName('button')[0].addEventListener('click', newGame);
+document.getElementById('playgame').addEventListener('submit', drawBoard);
+
